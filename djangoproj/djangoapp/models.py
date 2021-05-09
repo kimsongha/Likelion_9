e8cff4cbc120994ae5483bde895fb4da0543fb32
kimshongha <songha1024@naver.com>
2021-05-10 04:43:43 +0900

    Models

--- djangoproj/djangoapp/models.py
+++ djangoproj/djangoapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+#장고 공식문서 참고
+class Blog(models.Model): 
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    pub_date = models.DateField()
+    body = models.TextField()
+
+class School(models.Model):
+    name = models.CharField(max_length=30)
+    school = models.CharField(max_length=30)
+    student_id = models.IntegerField()
+    certificate_studentship = models.FileField()
